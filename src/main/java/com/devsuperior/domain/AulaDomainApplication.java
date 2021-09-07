4ca936a58a7866399eb93f0e3cd1b5e44dd96794
milinfic <paulo.cpa@hotmail.com>
2021-09-06 22:30:47 -0300

    primeiro commit - cópia readme do devsuperior e inicio do aplicativo

---
+++ src/main/java/com/devsuperior/domain/AulaDomainApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDomainApplication.class, args);
+	}
+
+}
